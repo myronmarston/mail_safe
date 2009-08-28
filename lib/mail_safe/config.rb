50377b47ddaef38334e284a1dd65bb10df0ed999
Myron Marston <myron.marston@gmail.com>
2009-08-28 08:34:46 -0700

    Added replacement address config setting.

--- lib/mail_safe/config.rb
+++ lib/mail_safe/config.rb
@@ -11,5 +11,15 @@ module MailSafe
         else raise InvalidConfigSettingError.new("internal_address_definition must be a Regexp or Proc, but was: #{internal_address_definition.class.to_s}")
       end
     end
+
+    cattr_accessor :replacement_address
+
+    def self.get_replacement_address(original_address)
+      case replacement_address
+        when String then replacement_address
+        when Proc   then replacement_address.call(original_address)
+        else raise InvalidConfigSettingError.new("replacement_address must be a String or Proc, but was: #{replacement_address.class.to_s}")
+      end
+    end
   end
 end
--- test/config_test.rb
+++ test/config_test.rb
@@ -31,12 +31,44 @@ class ConfigTest < Test::Unit::TestCase
 
   context 'When internal_address_definition is not set, #is_internal_address?' do
     setup do
-      assert_nil MailSafe::Config.internal_address_definition
+      MailSafe::Config.internal_address_definition = nil
     end
 
     should 'raise an error' do
       assert_raise MailSafe::InvalidConfigSettingError do
         MailSafe::Config.is_internal_address?('abc@foo.com')
+      end
+    end
+  end
+
+  context 'When replacement_address is set to a string, #get_replacement_address' do
+    setup do
+      MailSafe::Config.replacement_address = 'me@mydomain.com'
+    end
+
+    should 'return the configured replacement address' do
+      assert_equal 'me@mydomain.com', MailSafe::Config.get_replacement_address('you@example.com')
+    end
+  end
+
+  context 'When replacement_address is set to a proc, #get_replacement_address' do
+    setup do
+      MailSafe::Config.replacement_address = lambda { |address| "me+#{address.split('@').first}@mydomain.com" }
+    end
+
+    should 'return the configured replacement address' do
+      assert_equal 'me+you@mydomain.com', MailSafe::Config.get_replacement_address('you@example.com')
+    end
+  end
+
+  context 'When replacement_address is not set, #get_replacement_address' do
+    setup do
+      MailSafe::Config.replacement_address = nil
+    end
+
+    should 'raise an error' do
+      assert_raise MailSafe::InvalidConfigSettingError do
+        MailSafe::Config.get_replacement_address('you@example.com')
       end
     end
   end
