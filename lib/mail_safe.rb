8ba979f8883e16533e5b91f0c79ca59ac6123cc6
Myron Marston <myron.marston@gmail.com>
2010-01-27 08:24:50 -0800

    Only hook into ActionMailer's #deliver! method if we're using a version of AM before 3, since the rails 3 API is different.

--- lib/mail_safe.rb
+++ lib/mail_safe.rb
@@ -3,7 +3,10 @@ require 'action_mailer'
 require 'action_mailer/version'
 
 require 'mail_safe/config'
-require 'mail_safe/action_mailer'
 require 'mail_safe/address_replacer'
 
-ActionMailer::Base.send(:include, MailSafe::ActionMailer) unless ActionMailer::Base.ancestors.include?(MailSafe::ActionMailer)
+if ActionMailer::VERSION::MAJOR < 3
+  require 'mail_safe/action_mailer_for_rails2'
+  ActionMailer::Base.send(:include, MailSafe::ActionMailer) unless ActionMailer::Base.ancestors.include?(MailSafe::ActionMailer)
+else
+end
