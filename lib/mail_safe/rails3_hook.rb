c02d4821b6710d726b933c881f2bd298af4b5378
BananaNeil and Nick Novitski <engineering+banananeil+nick@sidereel.com>
2013-02-06 15:34:41 -0800

    Rails 3 compatibility

--- lib/mail_safe/rails3_hook.rb
+++ lib/mail_safe/rails3_hook.rb
@@ -3,7 +3,6 @@ require 'mail'
 module MailSafe
   class MailInterceptor
     def self.delivering_email(mail)
-      MailSafe::AddressReplacer.replace_external_addresses(mail) if mail
       MailSafe::Config.email_strategy.new.process_email(mail) if mail
     end
 
