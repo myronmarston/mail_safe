aab376618df6a8171461e77b4dfcf8d84c3eb33c
Juan José Conti <jjconti@gmail.com>
2014-09-01 12:44:04 -0300

    Avoid deprecation warning

--- lib/mail_safe.rb
+++ lib/mail_safe.rb
@@ -4,6 +4,8 @@ require 'action_mailer/version'
 require 'mail_safe/config'
 require 'mail_safe/address_replacer'
 
+I18n.config.enforce_available_locales = false
+
 if ActionMailer::VERSION::MAJOR < 3
   require 'mail_safe/rails2_hook'
 else
