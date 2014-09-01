e1fb1e35b70ea0fe24b98439c058d094123e6adb
Juan José Conti <jjconti@gmail.com>
2014-09-01 15:23:36 -0300

    Fix: avoid failing in some scenarios

--- lib/mail_safe.rb
+++ lib/mail_safe.rb
@@ -4,7 +4,10 @@ require 'action_mailer/version'
 require 'mail_safe/config'
 require 'mail_safe/address_replacer'
 
-I18n.config.enforce_available_locales = false
+# Set flag to avoid warning. Introduced in I18N v0.6.6
+if I18n.config.respond_to? :enforce_available_locales=
+  I18n.config.enforce_available_locales = false
+end
 
 if ActionMailer::VERSION::MAJOR < 3
   require 'mail_safe/rails2_hook'
