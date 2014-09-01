1ed95dff15270c406d256a8edaef9d1795520604
Juan José Conti <jjconti@gmail.com>
2014-09-01 15:25:21 -0300

    Fix: make tests action mailer 4 compilant

--- spec/mailers/test_mailer.rb
+++ spec/mailers/test_mailer.rb
@@ -5,29 +5,29 @@ class TestMailer < ActionMailer::Base
   end
 
   def plain_text_message(options)
-    setup_recipients(options)
-    mail(from: 'test@mailsafe.org',
-         subject: "Plain text Message Test") do |format|
+    setup = setup_recipients(options)
+    mail({from: 'test@mailsafe.org',
+         subject: "Plain text Message Test"}.update(setup)) do |format|
       format.text { render text: "Here is the message body." }
     end
   end
 
   def html_message(options)
-    setup_recipients(options)
+    setup = setup_recipients(options)
     body = "<p>Here is the message body.</p>"
     body = body.html_safe  if body.respond_to?(:html_safe)
-    mail(from: 'test@mailsafe.org',
-         subject: "Html Message Test") do |format|
+    mail({from: 'test@mailsafe.org',
+         subject: "Html Message Test"}.update(setup)) do |format|
       format.html { render text: body }
     end
   end
 
   def multipart_message(options)
-    setup_recipients(options)
+    setup = setup_recipients(options)
     html_body = "<p>Here is the message body.</p>"
     html_body = html_body.html_safe  if html_body.respond_to?(:html_safe)
-    mail(from: 'test@mailsafe.org',
-         subject: "Html Message Test") do |format|
+    mail({from: 'test@mailsafe.org',
+         subject: "Html Message Test"}.update(setup)) do |format|
       format.text { render text: "Here is the message body." }
       format.html { render text: html_body }
     end
@@ -36,8 +36,8 @@ class TestMailer < ActionMailer::Base
   protected
 
   def setup_recipients(options)
-    mail(to: options[:to],
-         cc: options[:cc],
-         bcc: options[:bcc])
+    {to: options[:to],
+     cc: options[:cc],
+     bcc: options[:bcc]}
   end
 end
